2e78cd57190d10de535bd19e1fc913642bc3f08d
Caleb Morse <calebmorse.code@gmail.com>
2026-08-21 19:04:06 -0500

    Add buttons for getting user selection, display round results using DOM event listeners

--- js-projects/rock-paper-scissors/app.js
+++ js-projects/rock-paper-scissors/app.js
@@ -1,94 +1,90 @@
-console.log('Hello, World!');
+// function getUserChoice() {
+//   let userChoice = prompt(
+//     'Please choose: Rock, Paper, or Scissors ',
+//   ).toLowerCase();
 
-// GET userChoice and STORE it in 'userSelection'
-// GET cpuChoice and STORE it in 'cpuSelection'
-// INIT userScore to 0
-// INIT cpuScore to 0
-// CREATE a FUNCTION to play a round
-// COMPARE userSelection to cpuSelection to determine winner or draw
-// IF winner, THEN update score of winner by 1
-// ELSE play another round
-// WHEN userScore or cpuScore === 5, the game is over and winner is declared
-// REPEAT playRound, UNTIL winner is declared
+//   return userChoice;
+// }
 
-function getUserChoice() {
-  let userChoice = prompt(
-    'Please choose: Rock, Paper, or Scissors ',
-  ).toLowerCase();
+function getComputerSelection() {
+  let selection = Math.floor(Math.random() * 3) + 1;
 
-  return userChoice;
-}
-
-function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 3) + 1;
-
-  if (computerChoice === 1) {
+  if (selection === 1) {
     return 'rock';
-  } else if (computerChoice === 2) {
+  } else if (selection === 2) {
     return 'paper';
   } else {
     return 'scissors';
   }
 }
 
-function isGameWinner(userScore, computerScore) {
-  if (userScore === computerScore) {
-    console.log('We have a draw!');
-    console.log(`FINAL SCORE: ${userScore} - ${computerScore}`);
-  } else if (userScore > computerScore) {
-    console.log(`User wins the game!`);
-    console.log(`FINAL SCORE: ${userScore} - ${computerScore}`);
+// function isGameWinner(userScore, computerScore) {
+//   if (userScore === computerScore) {
+//     console.log('We have a draw!');
+//     console.log(`FINAL SCORE: ${userScore} - ${computerScore}`);
+//   } else if (userScore > computerScore) {
+//     console.log(`User wins the game!`);
+//     console.log(`FINAL SCORE: ${userScore} - ${computerScore}`);
+//   } else {
+//     console.log(`Computer wins the game!`);
+//     console.log(`FINAL SCORE: ${computerScore} - ${userScore} `);
+//   }
+// }
+
+//   let userScore = 0;
+//   let computerScore = 0;
+//   let userSelection = getUserChoice();
+
+function playRound(userSelection, computerSelection) {
+  displayRoundSelections(userSelection, computerSelection);
+  isRoundWinner(userSelection, computerSelection);
+}
+
+function isRoundWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    displayRoundResult(userChoice, computerChoice);
+  } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+    displayRoundResult(userChoice, computerChoice);
+  } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+    displayRoundResult(userChoice, computerChoice);
+  } else if (userChoice === 'paper' && computerChoice === 'rock') {
+    displayRoundResult(userChoice, computerChoice);
   } else {
-    console.log(`Computer wins the game!`);
-    console.log(`FINAL SCORE: ${computerScore} - ${userScore} `);
+    displayRoundResult(computerChoice, userChoice);
   }
 }
 
-function playGame() {
-  let userScore = 0;
-  let computerScore = 0;
-  let userSelection = '';
-  let computerSelection = '';
+// isGameWinner(userScore, computerScore);
+const userSelectionBtns = document.querySelectorAll('button');
 
-  function playRound(userChoice, computerChoice) {
-    console.log('USER: ', userSelection);
-    console.log('COMPUTER: ', computerSelection);
-    console.log(isRoundWinner(userSelection, computerSelection));
-  }
-
-  function isRoundWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-      return `Draw! ${userChoice} and ${computerChoice} are the same!`;
-    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-      userScore++;
-      return `User wins! ${userChoice} beats ${computerChoice}!`;
-    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-      userScore++;
-      return `User wins! ${userChoice} beats ${computerChoice}!`;
-    } else if (userChoice === 'paper' && computerChoice === 'rock') {
-      userScore++;
-      return `User wins! ${userChoice} beats ${computerChoice}!`;
-    } else {
-      computerScore++;
-      return `Computer wins! ${computerChoice} beats ${userChoice}!`;
+userSelectionBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (roundSelections && roundSelections.hasChildNodes()) {
+      roundSelections.replaceChildren();
     }
-  }
-
-  let roundCount = 1;
-
-  while (roundCount < 6) {
-    userSelection = getUserChoice();
-    computerSelection = getComputerChoice();
-    console.log('ROUND: ', roundCount);
-    console.log(`USER SCORE: ${userScore} - COMPUTER SCORE: ${computerScore}`);
+    if (roundResult && roundResult.hasChildNodes()) {
+      roundResult.replaceChildren();
+    }
+    let userSelection = btn.id;
+    let computerSelection = getComputerSelection();
     playRound(userSelection, computerSelection);
-    roundCount++;
-  }
+  });
+});
 
-  console.log('---------------');
-  console.log('   GAME OVER');
-  console.log('---------------');
-  isGameWinner(userScore, computerScore);
+const roundSelections = document.querySelector('#round-selections');
+const roundResult = document.querySelector('#round-result');
+
+function displayRoundResult(winnerSelection, loserSelection) {
+  const para = document.createElement('p');
+  para.textContent =
+    winnerSelection !== loserSelection
+      ? `${winnerSelection} beats ${loserSelection}`
+      : `It's a draw! ${winnerSelection} and ${loserSelection} are the same.`;
+  roundResult.appendChild(para);
 }
 
-playGame();
+function displayRoundSelections(userSelection, computerSelection) {
+  const para = document.createElement('p');
+  para.textContent = `User selects ${userSelection} and Computer selects ${computerSelection}`;
+  roundSelections.appendChild(para);
+}
