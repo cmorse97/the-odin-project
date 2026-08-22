ca8f5892af8567bb54a1095ebdf9f31ba2507c49
Caleb Morse <calebmorse.code@gmail.com>
2026-08-21 22:50:22 -0500

    Add logic for updating score and checking for a winner

--- js-projects/rock-paper-scissors/app.js
+++ js-projects/rock-paper-scissors/app.js
@@ -1,61 +1,13 @@
-// function getUserChoice() {
-//   let userChoice = prompt(
-//     'Please choose: Rock, Paper, or Scissors ',
-//   ).toLowerCase();
-
-//   return userChoice;
-// }
-
-function getComputerSelection() {
-  let selection = Math.floor(Math.random() * 3) + 1;
-
-  if (selection === 1) {
-    return 'rock';
-  } else if (selection === 2) {
-    return 'paper';
-  } else {
-    return 'scissors';
-  }
-}
-
-// function isGameWinner(userScore, computerScore) {
-//   if (userScore === computerScore) {
-//     console.log('We have a draw!');
-//     console.log(`FINAL SCORE: ${userScore} - ${computerScore}`);
-//   } else if (userScore > computerScore) {
-//     console.log(`User wins the game!`);
-//     console.log(`FINAL SCORE: ${userScore} - ${computerScore}`);
-//   } else {
-//     console.log(`Computer wins the game!`);
-//     console.log(`FINAL SCORE: ${computerScore} - ${userScore} `);
-//   }
-// }
-
-//   let userScore = 0;
-//   let computerScore = 0;
-//   let userSelection = getUserChoice();
-
-function playRound(userSelection, computerSelection) {
-  displayRoundSelections(userSelection, computerSelection);
-  isRoundWinner(userSelection, computerSelection);
-}
-
-function isRoundWinner(userChoice, computerChoice) {
-  if (userChoice === computerChoice) {
-    displayRoundResult(userChoice, computerChoice);
-  } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-    displayRoundResult(userChoice, computerChoice);
-  } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-    displayRoundResult(userChoice, computerChoice);
-  } else if (userChoice === 'paper' && computerChoice === 'rock') {
-    displayRoundResult(userChoice, computerChoice);
-  } else {
-    displayRoundResult(computerChoice, userChoice);
-  }
-}
-
-// isGameWinner(userScore, computerScore);
 const userSelectionBtns = document.querySelectorAll('button');
+const userScoreBug = document.querySelector('#user-score');
+const computerScoreBug = document.querySelector('#computer-score');
+const roundSelections = document.querySelector('#round-selections');
+const roundResult = document.querySelector('#round-result');
+let userScore = 0;
+let computerScore = 0;
+
+userScoreBug.textContent = `${userScore}`;
+computerScoreBug.textContent = `${computerScore}`;
 
 userSelectionBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -71,15 +23,12 @@ userSelectionBtns.forEach((btn) => {
   });
 });
 
-const roundSelections = document.querySelector('#round-selections');
-const roundResult = document.querySelector('#round-result');
-
 function displayRoundResult(winnerSelection, loserSelection) {
   const para = document.createElement('p');
   para.textContent =
     winnerSelection !== loserSelection
-      ? `${winnerSelection} beats ${loserSelection}`
-      : `It's a draw! ${winnerSelection} and ${loserSelection} are the same.`;
+      ? `${winnerSelection.toUpperCase()} beats ${loserSelection.toUpperCase()}`
+      : `It's a draw! ${winnerSelection.toUpperCase()} and ${loserSelection.toUpperCase()} are the same.`;
   roundResult.appendChild(para);
 }
 
@@ -87,4 +36,58 @@ function displayRoundSelections(userSelection, computerSelection) {
   const para = document.createElement('p');
   para.textContent = `User selects ${userSelection} and Computer selects ${computerSelection}`;
   roundSelections.appendChild(para);
+}
+
+function getComputerSelection() {
+  let selection = Math.floor(Math.random() * 3) + 1;
+
+  if (selection === 1) {
+    return 'rock';
+  } else if (selection === 2) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+function isGameWinner(userScore, computerScore) {
+  if (userScore === 5 && computerScore < 5) {
+    roundResult.replaceChildren();
+    roundSelections.replaceChildren();
+    const result = `User wins the game!`;
+    roundResult.textContent = result;
+  } else if (computerScore === 5 && userScore < 5) {
+    roundResult.replaceChildren();
+    roundSelections.replaceChildren();
+    const result = 'Computer wins the game!';
+    roundResult.textContent = result;
+  }
+}
+
+function playRound(userSelection, computerSelection) {
+  displayRoundSelections(userSelection, computerSelection);
+  isRoundWinner(userSelection, computerSelection);
+  isGameWinner(userScore, computerScore);
+}
+
+function isRoundWinner(userSelection, computerSelection) {
+  if (userSelection === computerSelection) {
+    displayRoundResult(userSelection, computerSelection);
+  } else if (userSelection === 'rock' && computerSelection === 'scissors') {
+    displayRoundResult(userSelection, computerSelection);
+    userScore++;
+    userScoreBug.textContent = `${userScore}`;
+  } else if (userSelection === 'scissors' && computerSelection === 'paper') {
+    displayRoundResult(userSelection, computerSelection);
+    userScore++;
+    userScoreBug.textContent = `${userScore}`;
+  } else if (userSelection === 'paper' && computerSelection === 'rock') {
+    displayRoundResult(userSelection, computerSelection);
+    userScore++;
+    userScoreBug.textContent = `${userScore}`;
+  } else {
+    displayRoundResult(computerSelection, userSelection);
+    computerScore++;
+    computerScoreBug.textContent = `${computerScore}`;
+  }
 }
