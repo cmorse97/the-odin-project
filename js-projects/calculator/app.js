d45551a4f8ba60c449620f7d61ef5249c3827a43
Caleb Morse <calebmorse.code@gmail.com>
2026-09-08 15:32:48 -0500

    Create operate function that takes an operator function, and two numbers

--- js-projects/calculator/app.js
+++ js-projects/calculator/app.js
@@ -2,18 +2,31 @@
 console.log('Hello, World!');
 
 // Basic operations
-function addNums(num1, num2) {
-  console.log(num1 + num2);
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtractNums(num1, num2) {
-  console.log(num1 - num2);
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiplyNums(num1, num2) {
-  console.log(num1 * num2);
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divideNums(num1, num2) {
-  console.log(num1 / num2);
+function divide(num1, num2) {
+  return num1 / num2;
 }
+
+let firstOperand = 10;
+let secondOperand = 5;
+let operator = divide;
+
+function operate(operation, operand1, operand2) {
+  let result = operation(operand1, operand2);
+
+  console.log(result);
+  return result;
+}
+
+operate(operator, firstOperand, secondOperand);
