91cbb54f5c9edaafafc134cf7bb1cfc402b81b97
Caleb Morse <calebmorse.code@gmail.com>
2026-09-08 15:20:50 -0500

    Create basic math operator functions

--- js-projects/calculator/app.js
+++ js-projects/calculator/app.js
@@ -1,2 +1,19 @@
 // Calculator
 console.log('Hello, World!');
+
+// Basic operations
+function addNums(num1, num2) {
+  console.log(num1 + num2);
+}
+
+function subtractNums(num1, num2) {
+  console.log(num1 - num2);
+}
+
+function multiplyNums(num1, num2) {
+  console.log(num1 * num2);
+}
+
+function divideNums(num1, num2) {
+  console.log(num1 / num2);
+}
